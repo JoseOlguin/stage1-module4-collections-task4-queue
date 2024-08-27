a0bdd226388a8191c2532f5fed668cbae5256578
JoseOlguin <movistar.8331560462@gmail.com>
2024-08-27 10:20:09 -0600

    Commit for: stage1-module4-collections-task4-queue

--- src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
+++ src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
@@ -6,5 +6,28 @@ import java.util.Queue;
 
 public class ArrayDequeCreator extends PriorityQueue<String> {
     public ArrayDeque<Integer> createArrayDeque(Queue<Integer> firstQueue, Queue<Integer> secondQueue) {
+        ArrayDeque<Integer> arrayDeque = new ArrayDeque<>();
+
+        for (int i = 0; i < 2 && !firstQueue.isEmpty(); i++) {
+            arrayDeque.add(firstQueue.poll());
+        }
+
+        for (int i = 0; i < 2 && !secondQueue.isEmpty(); i++) {
+            arrayDeque.add(secondQueue.poll());
+        }
+
+        do {
+            firstQueue.add(arrayDeque.pollLast());
+            for (int i = 0; i < 2 && !firstQueue.isEmpty(); i++) {
+                arrayDeque.add(firstQueue.poll());
+            }
+
+            secondQueue.add(arrayDeque.pollLast());
+            for (int i = 0; i < 2 && !secondQueue.isEmpty(); i++) {
+                arrayDeque.add(secondQueue.poll());
+            }
+        } while (!firstQueue.isEmpty() && !secondQueue.isEmpty());
+
+        return arrayDeque;
     }
 }
--- src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
+++ src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
@@ -1,8 +1,26 @@
 package com.epam.collections.queue;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class DishOrderDeterminer {
     public List<Integer> determineDishOrder(int numberOfDishes, int everyDishNumberToEat) {
+        List<Integer> input = new ArrayList<>();
+        for (int i = 1; i <= numberOfDishes; i++) {
+            input.add(i);
+        }
+
+        int i = -1;
+        List<Integer> output = new ArrayList<>();
+        while (output.size() < numberOfDishes) {
+            int k = everyDishNumberToEat;
+            while (k-- > 0) {
+                i = ++i % input.size();
+            }
+            output.add(input.get(i));
+            input.remove(i--);
+        }
+
+        return output;
     }
 }
